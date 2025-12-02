0abd4bce7034b8149e37266259558bf06efd8278
Loui <Loui.fatum@gmail.com>
2025-12-02 12:49:45 +0100

    chart virker

--- fetch.js
+++ fetch.js
@@ -1,6 +1,33 @@
-  fetch('https://raw.githubusercontent.com/Lomi0001/FULLSTACK---Opgave-/refs/heads/main/optagelsesdata.json')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data.Optagelsesdata[0])
+document.addEventListener("DOMContentLoaded", () => {
 
-    })
+    fetch('https://raw.githubusercontent.com/Lomi0001/FULLSTACK---Opgave-/refs/heads/main/optagelsesdata.json')
+        .then(response => response.json())
+        .then(data => {
+
+            const ObjectData = data.Optagelsesdata[0];
+
+            // Ret stavefejl (Labels / Values)
+            const labels = ["Alder"];
+            const values = [Number(ObjectData.alder)];
+            const IndexData = ["id","køn","by"]
+
+
+
+            const ctx = document.querySelector('#chart').getContext('2d');
+
+            new Chart(ctx, {
+                type: "bar",
+                data: {
+                    labels: labels,
+                    datasets: [{
+                        label: `${ObjectData.uddannelse} (${ObjectData.by})`,
+                        data: values
+                    }]
+                },
+                options: {
+                    responsive: true
+                }
+            });
+
+        });
+});
